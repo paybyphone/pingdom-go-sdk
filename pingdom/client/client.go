4166a63293539c342535e9c8bb8f68357f318e1b
Chris Marchesi <cmarchesi@paybyphone.com>
2016-03-12 23:19:05 -0800

    Client scaffold
    Scaffolding for Client and default config provider bits. Also includes a
    small utility function for copying structs.

--- pingdom/client/client.go
+++ pingdom/client/client.go
@@ -1,1 +1,25 @@
 package client
+
+import (
+	"github.com/paybyphone/pingdom-go-sdk/pingdom"
+	"github.com/paybyphone/pingdom-go-sdk/util"
+)
+
+// Client encompasses a generic client object that is further extended by\
+// services. Any common configuration and functionality goes here.
+type Client struct {
+	// The configuration for this specific connection.
+	Config pingdom.Config
+}
+
+// New handles logic for either setting a conneciton based on supplied
+// configuration, or getting the configuration from a specific provider.
+func New(config pingdom.Config) *Client {
+	c := &Client{
+		Config: pingdom.DefaultConfigProvider(),
+	}
+	// Merge config objects.
+	util.SimpleCopyStruct(config, c.Config)
+
+	return c
+}
--- pingdom/pingdom.go
+++ pingdom/pingdom.go
@@ -1,5 +1,10 @@
 package pingdom
 
+import (
+	"os"
+	"strings"
+)
+
 // The pingdom API endpoint.
 const apiAddress = "https://api.pingdom.com"
 
@@ -19,4 +24,32 @@ type Config struct {
 
 	// The proxy config, if any.
 	Proxy string
+}
+
+// DefaultConfigProvider supplies a default configuration:
+//  * Endpoint defaults to https://api.pingdom.com. Proxy is unset.
+//  * EmailAddress defaults to PINGDOM_EMAIL_ADDRESS, if set, otherwise empty
+//  * Password defaults to PINGDOM_PASSWORD, if set, otherwise empty
+//  * AppKey defaults to PINGDOM_APP_KEY, if set, otherwise empty
+//
+// This essentially loads an initial config state for any given
+// API service.
+func DefaultConfigProvider() Config {
+	env := os.Environ()
+	cfg := Config{
+		Endpoint: apiAddress,
+	}
+
+	for _, v := range env {
+		d := strings.Split(v, "=")
+		switch d[0] {
+		case "PINGDOM_EMAIL_ADDRESS":
+			cfg.EmailAddress = d[1]
+		case "PINGDOM_PASSWORD":
+			cfg.Password = d[1]
+		case "PINGDOM_APP_KEY":
+			cfg.AppKey = d[1]
+		}
+	}
+	return cfg
 }
