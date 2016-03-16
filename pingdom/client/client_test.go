8614b1701048a7ed346343390676031e0c65fbd1
Chris Marchesi <cmarchesi@paybyphone.com>
2016-03-15 17:50:33 -0700

    More test work
    pingdom package now has working tests, working through the client
    package as well, will probably need to remove the util package and move
    that to Mergo.

--- pingdom/client/client_test.go
+++ pingdom/client/client_test.go
@@ -3,7 +3,10 @@ package client
 import (
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
+
+	"github.com/paybyphone/pingdom-go-sdk/pingdom"
 )
 
 const errorResponseText = `
@@ -15,6 +18,9 @@ const errorResponseText = `
   }
 }
 `
+
+const errorResponse = "Forbidden (403): Something went wrong! This string describes what happened."
+
 const okResponseText = `
  {
    "check": {
@@ -24,6 +30,14 @@ const okResponseText = `
  }
  `
 
+func pingdomConfig() pingdom.Config {
+	return pingdom.Config{
+		EmailAddress: "nobody@example.com",
+		Password:     "changeit",
+		AppKey:       "0123456789abcdefgh",
+	}
+}
+
 type okResponseCheckType struct {
 	ID   int
 	Name string
@@ -31,6 +45,27 @@ type okResponseCheckType struct {
 
 type okResponseType struct {
 	Check okResponseCheckType
+}
+
+func okResponse() okResponseType {
+	return okResponseType{
+		Check: okResponseCheckType{
+			ID:   138631,
+			Name: "My new HTTP check",
+		},
+	}
+}
+
+type queryStringDataTestBasicType struct {
+	ID   int    `url:"id"`
+	Name string `url:"name"`
+}
+
+func queryStringDataTestBasic() queryStringDataTestBasicType {
+	return queryStringDataTestBasicType{
+		ID:   1234,
+		Name: "My new HTTP check",
+	}
 }
 
 func newHTTPTestServer(f func(w http.ResponseWriter, r *http.Request)) *httptest.Server {
@@ -52,8 +87,61 @@ func httpOKTestServer() *httptest.Server {
 	})
 }
 
-func TestClientNewStatic(t *testing.T) {
+func TestClientNew(t *testing.T) {
+	c := New(pingdomConfig())
+
+	if c.Config.Endpoint != "https://api.pingdom.com" {
+		t.Fatalf("Expected Endpoint to be https://api.paybyphone.com, got %s", c.Config.Endpoint)
+	}
+	if c.Config.EmailAddress != "nobody@example.com" {
+		t.Fatalf("Expected EmailAddress to be nobody@example.com, got %s", c.Config.EmailAddress)
+	}
+	if c.Config.Password != "changeit" {
+		t.Fatalf("Expected Password to be changeit, got %s", c.Config.Password)
+	}
+	if c.Config.AppKey != "0123456789abcdefgh" {
+		t.Fatalf("Expected AppKey to be 0123456789abcdefgh, got %s", c.Config.AppKey)
+	}
 }
 
-func TestClientNewEnv(t *testing.T) {
+func TestClientSendRequestSuccess(t *testing.T) {
+	ts := httpOKTestServer()
+	defer ts.Close()
+	cfg := pingdomConfig()
+	cfg.Endpoint = ts.URL
+	c := New(cfg)
+	in := queryStringDataTestBasic()
+	out := okResponseType{}
+	err := c.SendRequest("GET", "/api/v2.0/test", &in, &out)
+
+	if err != nil {
+		t.Fatalf("Unexpected request error: %s", err)
+	}
+
+	expected := okResponse()
+
+	if reflect.DeepEqual(expected, out) == false {
+		t.Fatalf("expected %v, got %v", expected, out)
+	}
+}
+
+func TestClientSendRequestError(t *testing.T) {
+	ts := httpErrorTestServer()
+	defer ts.Close()
+	cfg := pingdomConfig()
+	cfg.Endpoint = ts.URL
+	c := New(cfg)
+	in := queryStringDataTestBasic()
+	out := okResponseType{}
+	err := c.SendRequest("GET", "/api/v2.0/test", &in, &out)
+
+	if err == nil {
+		t.Fatalf("Expected error, none found")
+	}
+
+	expected := errorResponse
+
+	if err.Error() != expected {
+		t.Fatalf("expected %s, got %s", expected, err)
+	}
 }
--- pingdom/pingdom_test.go
+++ pingdom/pingdom_test.go
@@ -5,17 +5,48 @@ import (
 	"testing"
 )
 
-func SetPingdomenv() {
+func setPingdomenv() {
 	os.Setenv("PINGDOM_EMAIL_ADDRESS", "nobody@example.com")
 	os.Setenv("PINGDOM_PASSWORD", "changeit")
 	os.Setenv("PINGDOM_APP_KEY", "abcdefgh0123456789")
 }
 
-func UnsetPingdomenv() {
+func unsetPingdomenv() {
 	os.Unsetenv("PINGDOM_EMAIL_ADDRESS")
 	os.Unsetenv("PINGDOM_PASSWORD")
 	os.Unsetenv("PINGDOM_APP_KEY")
 }
 
-func TestPingdomDefaultConfigProvider(t *testing.T) {
+func TestPingdomDefaultConfigProviderWithEnv(t *testing.T) {
+	setPingdomenv()
+	c := DefaultConfigProvider()
+	if c.Endpoint != "https://api.pingdom.com" {
+		t.Fatalf("Expected Endpoint to be https://api.paybyphone.com, got %s", c.Endpoint)
+	}
+	if c.EmailAddress != "nobody@example.com" {
+		t.Fatalf("Expected EmailAddress to be nobody@example.com, got %s", c.EmailAddress)
+	}
+	if c.Password != "changeit" {
+		t.Fatalf("Expected Password to be changeit, got %s", c.Password)
+	}
+	if c.AppKey != "abcdefgh0123456789" {
+		t.Fatalf("Expected AppKey to be abcdefgh0123456789, got %s", c.AppKey)
+	}
+}
+
+func TestPingdomDefaultConfigProviderNoEnv(t *testing.T) {
+	unsetPingdomenv()
+	c := DefaultConfigProvider()
+	if c.Endpoint != "https://api.pingdom.com" {
+		t.Fatalf("Expected Endpoint to be https://api.paybyphone.com, got %s", c.Endpoint)
+	}
+	if c.EmailAddress != "" {
+		t.Fatalf("Expected EmailAddress to be empty, got %s", c.EmailAddress)
+	}
+	if c.Password != "" {
+		t.Fatalf("Expected Password to be empty, got %s", c.Password)
+	}
+	if c.AppKey != "" {
+		t.Fatalf("Expected AppKey to be empty, got %s", c.AppKey)
+	}
 }
